0a49917f34f25c1d47148333d057382f1a247a87
bodrini <pat.gor4@gmail.com>
2026-01-22 14:23:56 +0100

    chore:eslint test

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const PORT = 3000;
 
 app.use('/api', teamsRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
